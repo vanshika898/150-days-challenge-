93544476944a24c62d122b794920096e3bdf7002
tanisha533 <140799888+tanisha533@users.noreply.github.com>
2023-07-29 16:30:05 +0000

    main

---
+++ day2.java
@@ -0,0 +1,10 @@
+/* Sum of variables ( a & b) in java */
+
+public class day2{
+    public static void main(String arg[]){
+        int a= 98;
+        int b= 112;
+        int sum= a+b;
+        System.out.println(sum);
+    }
+}
