0ec38ea9b1a4618c992db06edcf81952eedd2550
tanisha533 <140799888+tanisha533@users.noreply.github.com>
2023-08-18 17:52:03 +0000

    main

---
+++ DAY-2(Conditional statement)/q1.java
@@ -0,0 +1,14 @@
+//Switch and break 
+class q1 {
+    public static void main(String[] args) {
+        int num=2;
+        switch (num){
+        case 1:System.out.println("Monday");
+        break;
+        case 2:System.out.println("Tuesday");
+        break;
+        case 3:System.out.println("Wednesday");
+        default:
+        System.out.println("oho nooo");}
+    }    
+}
---
+++ DAY-2(Conditional statement)/q2.java
@@ -0,0 +1,11 @@
+// conditional statement
+class q2{
+    public static void main(String[] args) {
+        double temp=103.5;
+        if (temp>100){
+            System.out.println("You have a fever");}
+        else{
+            System.out.println("You don't have fever");
+        }
+        }
+    }    
